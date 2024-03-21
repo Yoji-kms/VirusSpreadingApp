4970ea49e9231052f8be6ee1d945d20f281f22f8
Mashnin Egor <mashnin_yegor@mail.ru>
2024-03-21 18:13:34 +0300

    Initial Commit

---
+++ VirusSpreadingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirusSpreadingApp
+//
+//  Created by Yoji on 21.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
